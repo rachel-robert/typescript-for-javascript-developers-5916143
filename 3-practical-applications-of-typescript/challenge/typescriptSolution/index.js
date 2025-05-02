192cc78264fbf70fe5960ad78f98a42823179e9f
rachel-robert <rachelrobert109@gmail.com>
2025-05-02 17:05:31 +0000

    Complete Unit 3

--- 3-practical-applications-of-typescript/challenge/typescriptSolution/index.js
+++ 3-practical-applications-of-typescript/challenge/typescriptSolution/index.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const user_1 = require("./user");
 (0, user_1.addUser)({ id: 1, name: 'Jeema', email: 'jeema@example.com' });
 (0, user_1.addUser)({ id: 2, name: 'Ali', email: 'ali@example.com' });
-console.log((0, user_1.getUser)(1));
+console.log((0, user_1.getUser)(1)); // Should return the user object for Jeema
 console.log((0, user_1.getUser)(3)); // Should return 'User not found'
