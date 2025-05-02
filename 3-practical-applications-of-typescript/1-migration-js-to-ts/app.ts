0fe9b0727a25a5c7d6071dc203fb2267507a3030
rachel-robert <rachelrobert109@gmail.com>
2025-05-02 16:33:12 +0000

    Complete Unit 3.1

--- 3-practical-applications-of-typescript/1-migration-js-to-ts/app.ts
+++ 3-practical-applications-of-typescript/1-migration-js-to-ts/app.ts
@@ -1,7 +1,7 @@
 import { add, subtract } from './mathUtils';
 
-const result1 = add(10, 5);
-const result2 = subtract(10, 5);
+const result1: number = add(10, 5);
+const result2: number = subtract(10, 5);
 
 console.log('Addition:', result1);
 console.log('Subtraction:', result2);
--- 3-practical-applications-of-typescript/1-migration-js-to-ts/mathUtils.js
+++ 3-practical-applications-of-typescript/1-migration-js-to-ts/mathUtils.js
@@ -1,11 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.subtract = exports.add = void 0;
+exports.add = add;
+exports.subtract = subtract;
 function add(a, b) {
     return a + b;
 }
-exports.add = add;
 function subtract(a, b) {
     return a - b;
 }
-exports.subtract = subtract;
